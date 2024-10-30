908f38781d3a3ab5d8c1b18657e9726f5dc110f0
ThaiHoang600 <th1169895@gmail.com>
2024-10-30 18:28:47 +0700

    Initial commit
    hehe

---
+++ Code/HienThiHpAB.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HienThiHpAB : MonoBehaviour
+{
+    public Slider mySlider;
+    public void OnSliderValueChange(float value)
+    {
+        Debug.Log("Slider Value: " +20);
+    }
+}
